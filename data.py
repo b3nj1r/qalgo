b960629e5df463d69ef4ecb255274cab728a857e
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2020-12-03 14:27:45 -0800

    proper candlestick visualization. fixes #1

--- data.py
+++ data.py
@@ -1,2 +1,39 @@
 # handle and manage historical data
-# prepare for algorithms
+
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+# get security history
+sec = yf.Ticker('TSLA')
+sec_hist = sec.history(period='60d')
+sec_hist.reset_index(inplace=True)
+
+# get bars
+origin = sec_hist['Date'][0]
+sec_hist['bar'] = sec_hist['Date'].map(lambda bar: (bar-origin).days)
+
+def candlestick(ax,data,pos,neg):
+    """Read price information to create a candlestick, return the axis with the added candle stick."""
+    if data['Close'] > data['Open']:
+        color = pos
+    else:
+        color = neg
+    # create wick 
+    ax.plot([data['bar'],data['bar']],[data['Low'],data['High']], linewidth=1, color=color)
+
+    # create body
+    rect = mpl.patches.Rectangle((data['bar'] - 0.25, data['Open']), 0.5, (data['Close'] - data['Open']), facecolor=color, edgecolor='none', linewidth=0, zorder=3)
+    
+    # add rectangle to axis
+    ax.add_patch(rect)
+    return ax
+
+def draw_chart(ax, data, pos='white', neg='black'):
+    """Iterate through data points, return axis with all added candlesticks"""
+    for bar in range(data.shape[0]):
+        ax = candlestick(ax,data.iloc[bar],pos,neg)
+    return ax
+
+
--- visualize.py
+++ visualize.py
@@ -2,13 +2,30 @@
 import data
 import process
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pandas as pd
 
-df = pd.read_csv('data.csv') 
 
-# retrieve close historical data 
-df['Close'][:50].plot()
-# plot last 5 close points from .csv data
+# plot parameters
+mpl.rcParams['axes.facecolor'] = '#2a2a2a'
+
+# create empty axis and figure
+fig = plt.figure(figsize=(10,5),facecolor='white')
+ax = fig.add_subplot(111)
+
+# draw chart from data
+ax = data.draw_chart(ax, data.sec_hist, 'green','red')
+
+# plot grid
+ax.grid(linestyle='-', linewidth=1, color='#4a4a4a', zorder=1)
+
+# axis lable formatting
+ax.set_xticks(list(data.sec_hist['bar'])[::5])
+ax.set_xticklabels(list(data.sec_hist['Date'].dt.strftime('%m-%d'))[::5])
+
+# print security information
+print(data.sec_hist.head())
+
+# save and display plot
 plt.savefig('out.png')
 plt.show()
