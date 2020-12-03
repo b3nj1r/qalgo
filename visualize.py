3ea4465b218f735ef9c63848d640a29bf9ccb638
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2020-12-03 10:15:35 -0800

    visualizing temporary historical data

--- visualize.py
+++ visualize.py
@@ -1,1 +1,14 @@
 # visualization of trading indicators
+import data
+import process
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data.csv') 
+
+# retrieve close historical data 
+df['Close'][:50].plot()
+# plot last 5 close points from .csv data
+plt.savefig('out.png')
+plt.show()
