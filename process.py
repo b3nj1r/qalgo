e3e91149acfa0c04db681989a62fe6946643878e
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2020-12-04 10:13:32 -0800

    update sma src to be color dependant

--- process.py
+++ process.py
@@ -3,9 +3,12 @@
 import numpy as np
 
 def sma(ax, data, color):
-    #plt.plot([x1,x2], [y1,y2])
-    #ax.plot([data['bar']],[data['Close'].mean()], linewidth=10, color=color)
+    
+    # create new collumn and fill it with the mean for the last 3 bars 
+    
     data['sma3'] = data.iloc[:,1].rolling(window=3).mean()
+    # plot new data
+    ax.plot(data['bar'],data['sma3'],color=color)
+    
     print(data.head())
-    ax.plot(data['bar'],data['sma3'])
     return ax
