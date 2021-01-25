7353a881558466363c8120717f17244eefd15a71
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2021-01-25 07:19:45 -0800

    added dark theme visuals and updated project directory

--- src/visualize.py
+++ src/visualize.py
@@ -7,18 +7,19 @@ import matplotlib.pyplot as plt
 
 
 # plot parameters
-mpl.rcParams['axes.facecolor'] = '#2a2a2a'
+plt.style.use('dark_background')
 
 # create empty axis and figure
-fig = plt.figure(figsize=(10,5),facecolor='white')
+fig = plt.figure(figsize=(10,5))
 ax = fig.add_subplot(111)
 
 # draw chart from data
 ax = data.draw_chart(ax, data.sec_hist, 'green','red')
 ax = process.sma(15, ax, data.sec_hist, 'yellow')
 ax = process.sma(5, ax, data.sec_hist, 'white')
+
 # plot grid
-ax.grid(linestyle='-', linewidth=1, color='#4a4a4a', zorder=1)
+ax.grid(linestyle='-', linewidth=1, color='#2a2a2a', zorder=1)
 
 # axis lable formatting
 ax.set_xticks(list(data.sec_hist['bar'])[::5])
@@ -29,5 +30,5 @@ plt.xticks(rotation=90)
 print(data.sec_hist.head())
 
 # save and display plot
-plt.savefig('sma.png')
+plt.savefig('../sma.png')
 plt.show()
